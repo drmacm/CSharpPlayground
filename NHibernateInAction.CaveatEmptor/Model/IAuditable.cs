514cf26f152a3c0a6533a40f32579b7da0e7c26f
mdrmac <drmac.mladen@gmail.com>
2018-07-28 13:55:40 +0200

    Cleaning up

--- NHibernateInAction.CaveatEmptor/Model/IAuditable.cs
+++
@@ -1,8 +0,0 @@
-namespace NHibernateInAction.CaveatEmptor.Model
-{
-    /// <summary> A marker interface for auditable persistent domain classes. </summary>
-    public interface IAuditable
-    {
-        long Id { get; }
-    }
-}
